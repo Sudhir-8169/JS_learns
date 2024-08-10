bbbe9ec18a0f29c676e1cb48ef9c0830aed6de93
gitikaverma83 <gitikaverma837@gmail.com>
2024-08-11 00:04:47 +0530

    i have added the course file till projects

---
+++ 04_higher_order_array_loops/reduce.js
@@ -0,0 +1,10 @@
+//   making a single value of array
+
+// const nums=[10,20,3]
+// total=nums.reduce((a,current)=>{
+//     console.log(current);
+    
+//     return a+current
+    
+// },0)
+// console.log(total);
